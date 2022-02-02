efbd8a90ef73213b4f5a167914d7f8d816b9b9e1
babybeb <dasomchoi52@gmail.com>
2022-02-02 21:04:23 +0900

    Initial commit

---
+++ src/main/java/babybeb/usersusedbookstore/UsersUsedBookStoreApplication.java
@@ -0,0 +1,13 @@
+package babybeb.usersusedbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersUsedBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersUsedBookStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/babybeb/usersusedbookstore/UsersUsedBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package babybeb.usersusedbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersUsedBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

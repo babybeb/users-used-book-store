e3cf7e90de993e1fd81710403566909d75a924d7
ggeggrgg <ggeggrgg@naver.com>
2022-04-05 19:29:21 +0900

    make Sale inherit from Trade

--- src/main/java/babybeb/usersusedbookstore/domain/Sale.java
+++ src/main/java/babybeb/usersusedbookstore/domain/Sale.java
@@ -4,22 +4,9 @@ import lombok.Getter;
 
 import javax.persistence.*;
 
-import static javax.persistence.FetchType.*;
-
 @Entity
+@DiscriminatorValue("SALE")
 @Getter
-public class Sale {
-
-    @Id @GeneratedValue
-    @Column(name = "sale_id")
-    private Long id;
-
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name="member_id")
-    private Member member;
-
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "item_id")
-    private Item item;
+public class Sale extends Trade{
 
 }

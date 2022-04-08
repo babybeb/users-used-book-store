dcf13a88d7f558f7386defe66542bac830fea8f6
babybeb <dasomchoi52@gmail.com>
2022-04-08 23:50:41 +0900

    Fix Item domain

--- src/main/java/babybeb/usersusedbookstore/domain/Item.java
+++ src/main/java/babybeb/usersusedbookstore/domain/Item.java
@@ -1,6 +1,8 @@
 package babybeb.usersusedbookstore.domain;
 
+import babybeb.usersusedbookstore.domain.dto.MemberDto;
 import java.awt.print.Book;
+import java.util.ArrayList;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -8,8 +10,9 @@ import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+import javax.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,7 +28,7 @@ public class Item {
     private Long id;
     
     @ManyToOne(fetch = FetchType.LAZY)
-    @Column(name = "book_id")
+    @JoinColumn(name = "book_id")
     private Book book;
     
     private int price;
@@ -40,6 +43,21 @@ public class Item {
     @Enumerated(EnumType.STRING)
     private DealStatus dealStatus;
     
-    @OneToOne(fetch = FetchType.LAZY)
-    private ChatRoom chatRoom;
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private List<ChatRoom> chatRooms = new ArrayList<>();
+    
+    public static Item createItem(Book book, int price, String itemCondition, ImageInfo imageInfo) {
+        Item item = new Item();
+        item
+        
+        return item;
+    }
+    
+    public Item(Book book, int price, ItemCondition itemCondition, ImageInfo imageInfo) {
+        this.book = book;
+        this.price = price;
+        this.itemCondition = itemCondition;
+        this.imageInfo = imageInfo;
+    }
 }

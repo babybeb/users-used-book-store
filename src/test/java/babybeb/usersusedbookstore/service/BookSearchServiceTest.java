6305430e640c6c7d3b9e7f644e785f505c0ace69
babybeb <dasomchoi52@gmail.com>
2022-04-18 03:36:39 +0900

    Fix BookSearchServiceTest to add 책_지은이로_정보_검색하기()

--- src/test/java/babybeb/usersusedbookstore/service/BookSearchServiceTest.java
+++ src/test/java/babybeb/usersusedbookstore/service/BookSearchServiceTest.java
@@ -1,8 +1,5 @@
 package babybeb.usersusedbookstore.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -50,17 +47,19 @@ class BookSearchServiceTest {
         Assertions.assertThat(bookInfos.get(0).getTitle())
             .containsIgnoringCase(searchTitle);
     }
-
-//    @Test
-//    public void 책_지은이로_정보_검색하기() throws Exception {
-//
-//        //given
-//        String searchAuthor = "김영한";
-//
-//        //when
-//        List<BookDto> bookInfos = bookSearchService.searchBookInfosByAuthor(searchAuthor);
-//
-//        //then
-//        assertEquals("검색결과 첫번째 책의 지은이는 searchWord과 같다", searchAuthor, bookInfos.get(0).getTitle());
-//    }
+    
+    @Test
+    @DisplayName("검색결과 첫번째 책의 지은이는 searchAuthor를 포함한다")
+    public void 책_지은이로_정보_검색하기() throws Exception {
+        
+        //given
+        String searchAuthor = "김영한";
+        
+        //when
+        List<BookDto> bookInfos = bookSearchService.searchBookInfosByAuthor(searchAuthor);
+        
+        //then
+        Assertions.assertThat(bookInfos.get(0).getAuthor())
+            .containsIgnoringCase(searchAuthor);
+    }
 }

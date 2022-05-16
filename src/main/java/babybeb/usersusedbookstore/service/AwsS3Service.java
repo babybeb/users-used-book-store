8cff9f95a46941dd104268e103a3391e7e63693e
babybeb <dasomchoi52@gmail.com>
2022-05-16 23:20:07 +0900

    Add AwsS3Service

---
+++ src/main/java/babybeb/usersusedbookstore/service/AwsS3Service.java
@@ -0,0 +1,132 @@
+package babybeb.usersusedbookstore.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AwsS3Service {
+    
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+    
+    private final AmazonS3Client amazonS3Client;
+    
+    /**
+     * MultipartFile List를 매개변수로 받아 S3에 업로드하는 메서드
+     *
+     * @param multipartFiles
+     * @return fileNameList
+     * @throws IOException
+     */
+    public List<String> uploadImages(List<MultipartFile> multipartFiles) throws IOException {
+        List<String> fileNameList = new ArrayList<>();
+        
+        for (MultipartFile multipartFile : multipartFiles) {
+            if (!multipartFile.isEmpty()) {
+                fileNameList.add(uploadImage(multipartFile));
+            }
+        }
+        
+        return fileNameList;
+    }
+    
+    /**
+     * fileName List를 매개변수로 받아 해당 파일들을 S3 버킷에서 지우는 메서드
+     *
+     * @param fileNames
+     */
+    public void deleteImages(List<String> fileNames) {
+        for (String fileName : fileNames) {
+            deleteImage(fileName);
+        }
+    }
+    
+    /**
+     * MultipartFile 1개를 S3에 업로드하는 메서드
+     *
+     * @param multipartFile
+     * @return fileName
+     */
+    private String uploadImage(MultipartFile multipartFile) {
+        if (multipartFile.isEmpty()) {
+            return null;
+        }
+        
+        String fileName = createFileName(multipartFile.getOriginalFilename());
+        putS3(multipartFile, fileName);
+        
+        return fileName;
+    }
+    
+    /**
+     * MultipartFile과 String을 매개변수로 받아 S3에 public 읽기 권한으로 put하는 메서드
+     *
+     * @param uploadFile
+     * @param fileName
+     */
+    private void putS3(MultipartFile uploadFile, String fileName) {
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(uploadFile.getSize());
+        objectMetadata.setContentType(uploadFile.getContentType());
+        
+        try (InputStream inputStream = uploadFile.getInputStream()) {
+            amazonS3Client.putObject(
+                new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+        } catch (IOException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드 실패");
+        }
+    }
+    
+    /**
+     * originalFileName을 매개변수로 받아 uuid로 file의 이름을 생성한 것에 원래 파일의 확장자를 더해 반환하는 메서드
+     *
+     * @param originalFileName
+     * @return
+     */
+    private String createFileName(String originalFileName) {
+        String ext = extractExt(originalFileName);
+        String uuid = UUID.randomUUID().toString();
+        return uuid + "." + ext;
+    }
+    
+    /**
+     * originalFileName을 매개변수로 받아 해당 파일의 확장자를 구하는 메서드
+     *
+     * @param originalFileName
+     * @return
+     */
+    private String extractExt(String originalFileName) {
+        try {
+            return originalFileName.substring(originalFileName.lastIndexOf("." + 1));
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "파일 확장자 오류");
+        }
+    }
+    
+    /**
+     * fileName 1개를 매개변수로 받아 해당 파일을 S3 버킷에서 지우는 메서드
+     *
+     * @param fileName
+     */
+    private void deleteImage(String fileName) {
+        amazonS3Client.deleteObject(new DeleteObjectRequest(bucket, fileName));
+    }
+}

87fbab5ec7a63771d80ebe2fd940ffb67f085996
babybeb <dasomchoi52@gmail.com>
2022-05-17 20:21:57 +0900

    Add ItemRepository

---
+++ src/main/java/babybeb/usersusedbookstore/repository/ItemRepository.java
@@ -0,0 +1,32 @@
+package babybeb.usersusedbookstore.repository;
+
+import babybeb.usersusedbookstore.domain.Item;
+import java.util.List;
+import javax.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+    
+    private final EntityManager em;
+    
+    public Long save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item);
+        } else {
+            em.merge(item);
+        }
+        return item.getId();
+    }
+    
+    public Item findOne(Long id) {
+        return em.find(Item.class,id);
+    }
+    
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+            .getResultList();
+    }
+}

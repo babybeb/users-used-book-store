bc183717f51ffb831e8684c93684927991abe4c0
ggeggrgg <ggeggrgg@naver.com>
2022-05-21 00:12:22 +0900

    Del addSale, updateSale AND Add findOne

--- src/main/java/babybeb/usersusedbookstore/service/SaleService.java
+++ src/main/java/babybeb/usersusedbookstore/service/SaleService.java
@@ -17,31 +17,31 @@ public class SaleService {
     /**
      * 판매 등록하기
      */
-    public Long addSale(Long memberId, BookDto dto, int itemPrice, ItemCondition condition, ImageInfo image){
-        //맴버 조회
-        Member member = memberRepository.findOne(memberId);
-
-        //판매상품 정보 입력
-        Book book = new Book(dto.getIsbn(), dto.getTitle(), dto.getPrice(), dto.getPublisher(), dto.getAuthor(),
-                dto.getPage(), dto.getKdc());
-
-        //판매상품 생성
-        Item item = Item.createItem(book, itemPrice, condition, image);
-
-        //판매 생성
-        Sale sale = Sale.createSale(member, item);
-
-        //판매정보 저장
-        saleRepository.save(sale);
-        return sale.getId();
-    }
+//    public Long addSale(Long memberId, BookDto dto, int itemPrice, ItemCondition condition, ImageInfo image){
+//        //맴버 조회
+//        Member member = memberRepository.findOne(memberId);
+//
+//        //판매상품 정보 입력
+//        Book book = new Book(dto.getIsbn(), dto.getTitle(), dto.getPrice(), dto.getPublisher(), dto.getAuthor(),
+//                dto.getPage(), dto.getKdc());
+//
+//        //판매상품 생성
+//        Item item = Item.createItem(book, itemPrice, condition, image);
+//
+//        //판매 생성
+//        Sale sale = Sale.createSale(member, item);
+//
+//        //판매정보 저장
+//        saleRepository.save(sale);
+//        return sale.getId();
+//    }
 
     /**
-     * 판매 수정하기
+     * 판매 조회하기
      */
-//    public void updateSale(Long saleId){
-//        Sale sale = saleRepository.findOne(saleId);
-//    }
+    public Sale findOne(Long saleId){
+        return saleRepository.findOne(saleId);
+    }
 
     /**
      * 판매 삭제하기

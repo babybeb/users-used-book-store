9c82a41d2eebaca29e2e2f3f2212995ed7c865c6
babybeb <dasomchoi52@gmail.com>
2022-05-23 22:59:25 +0900

    Fix findItem PostMapping to GetMapping

--- src/main/java/babybeb/usersusedbookstore/api/ItemController.java
+++ src/main/java/babybeb/usersusedbookstore/api/ItemController.java
@@ -4,6 +4,7 @@ import babybeb.usersusedbookstore.domain.dto.ItemResponse;
 import babybeb.usersusedbookstore.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,7 @@ public class ItemController {
     
     private final ItemService itemService;
     
-    @PostMapping("/{item_id}")
+    @GetMapping("/{item_id}")
     public ResponseEntity<ItemResponse> findItem(@PathVariable("item_id") Long itemId) {
             
             return ResponseEntity.ok(ItemResponse.toItemResponse(itemService.findById(itemId)));

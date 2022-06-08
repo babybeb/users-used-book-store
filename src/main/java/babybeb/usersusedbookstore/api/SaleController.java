9aa646cadbdb2f55541aee1fb83c7daa45c47707
ggeggrgg <ggeggrgg@naver.com>
2022-06-09 06:29:16 +0900

    Fix save Sale method in SaleController

--- src/main/java/babybeb/usersusedbookstore/api/SaleController.java
+++ src/main/java/babybeb/usersusedbookstore/api/SaleController.java
@@ -1,6 +1,8 @@
 package babybeb.usersusedbookstore.api;
 
+import babybeb.usersusedbookstore.api.dto.sale.*;
 import babybeb.usersusedbookstore.domain.*;
+import babybeb.usersusedbookstore.domain.dealarea.DealArea;
 import babybeb.usersusedbookstore.service.BookDto;
 import babybeb.usersusedbookstore.service.ItemService;
 import babybeb.usersusedbookstore.service.MemberService;
@@ -30,19 +32,22 @@ public class SaleController {
     //판매 등록 API
     @PostMapping("sale/{memberId}/new")
     public SaveSaleResponse saveSale(
-            @PathVariable("memberId") Long id,
+            @PathVariable("memberId") Long memberId,
             @RequestBody @Valid SaveSaleRequest request){
         //판매 등록 시간 저장 (yyyy-MM-dd T HH:mm:ss)
         LocalDateTime date = LocalDateTime.now();
 
-        //책정보
-        BookDto book = new BookDto(request.isbn, request.title, request.bookPrice, request.publisher,
-                request.author, request.page, request.kdc);
+        //책정보(ISBN, 제목, 가격, 출판사, 저자, 페이지, Kdc, 카테고리)
+        BookDto book = new BookDto(request.getIsbn(), request.getTitle(), request.getBookPrice(),
+                request.getPublisher(), request.getAuthor(), request.getPage(), request.getKdc(),
+                request.getCategory());
 
-        //세일 생성
-        Long saleId = saleService.addSale(id, book, request.price, request.itemCondition, date);
-        Sale sale = saleService.findOne(saleId);
-        return new SaveSaleResponse(sale.getItem().getBook().getTitle(), sale.getItem().getItemPrice());
+        //세일 생성(회원아이디, 책정보, 판매가격, 책 상태, 등록날짜, 거래지역)
+        Long itemId = saleService.addSale(memberId, book, request.getPrice(), request.getItemCondition(), date,
+                request.getDealArea());
+
+        //이미지 등록을 위한 itemId 값 반환
+        return new SaveSaleResponse(itemId);
     }
 
     //판매 삭제 API
@@ -61,7 +66,7 @@ public class SaleController {
             @Valid ChangeStatusRequest request,
             @PathVariable("saleId") Long saleId){
         Sale findSale = saleService.findOne(saleId);
-        findSale.getItem().changeDealStatus(request.dealStatus);
+        findSale.getItem().changeDealStatus(request.getDealStatus());
         return new ChangeStatusResponse(findSale.getItem().getDealStatus());
     }
 
@@ -70,68 +75,8 @@ public class SaleController {
     public RateBuyerResponse RateBuyer(
             @Valid RateBuyerRequest request,
             @PathVariable("saleId") Long saleId){
-        saleService.rateBuyer(saleId, request.rate, request.buyerId);
-        Member buyer = memberService.findOne(request.buyerId);
-        return new RateBuyerResponse(buyer.getNickname(), request.rate);
-    }
-
-    /**
-     * DATA 부분
-     */
-    @Data
-    static class SaveSaleRequest{
-        //받아야 하는 값
-        private int price;
-        private ItemCondition itemCondition;
-        //Book 데이터
-        private String isbn;
-        private String title;
-        private int bookPrice;
-        private String publisher;
-        private String author;
-        private int page;
-        private String kdc;
-    }
-
-    @Data
-    @AllArgsConstructor
-    static class SaveSaleResponse{
-        //반환값
-        private String bookTitle;
-        private int price;
-    }
-
-    @Data
-    @AllArgsConstructor
-    static class DeleteSaleResponse{
-        //반환값
-        private String bookTitle;
-    }
-
-    @Data
-    static class ChangeStatusRequest{
-        //받아야 하는 값
-        private String dealStatus;
-    }
-
-    @Data
-    @AllArgsConstructor
-    static class ChangeStatusResponse{
-        //반환값
-        private DealStatus dealStatus;
-    }
-
-    @Data
-    static class RateBuyerRequest{
-        //받아야 하는 값
-        private Long buyerId;
-        private int rate;
-    }
-
-    @Data
-    @AllArgsConstructor
-    static class RateBuyerResponse{
-        private String buyerNickname;
-        private int rate;
+        saleService.rateBuyer(saleId, request.getRate(), request.getBuyerId());
+        Member buyer = memberService.findOne(request.getBuyerId());
+        return new RateBuyerResponse(buyer.getNickname(), request.getRate());
     }
 }

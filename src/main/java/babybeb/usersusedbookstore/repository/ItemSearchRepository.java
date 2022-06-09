3dd9b95bad6551f02576e86eeb4d2db3decb1d17
babybeb <dasomchoi52@gmail.com>
2022-06-09 22:32:14 +0900

    Fix ItemSearchRepository

--- src/main/java/babybeb/usersusedbookstore/repository/ItemSearchRepository.java
+++ src/main/java/babybeb/usersusedbookstore/repository/ItemSearchRepository.java
@@ -1,10 +1,19 @@
 package babybeb.usersusedbookstore.repository;
 
+import babybeb.usersusedbookstore.domain.Item;
+import java.util.List;
+import javax.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class ItemSearchRepository {
-
+    
+    private final EntityManager em;
+    
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+            .getResultList();
+    }
 }

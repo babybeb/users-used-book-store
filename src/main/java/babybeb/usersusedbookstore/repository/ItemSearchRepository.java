6443b3d936e8826147e57b4e7135e18c30092b4c
babybeb <dasomchoi52@gmail.com>
2022-06-09 20:49:39 +0900

    Add ItemSearchRepository

---
+++ src/main/java/babybeb/usersusedbookstore/repository/ItemSearchRepository.java
@@ -0,0 +1,10 @@
+package babybeb.usersusedbookstore.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemSearchRepository {
+
+}

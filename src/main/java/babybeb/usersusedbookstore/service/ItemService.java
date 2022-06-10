57c0ce9a6235c718cd489ad4b28e819fbca532b0
babybeb <dasomchoi52@gmail.com>
2022-06-10 21:27:27 +0900

    Fix ItemService to add changeDealStatus method

--- src/main/java/babybeb/usersusedbookstore/service/ItemService.java
+++ src/main/java/babybeb/usersusedbookstore/service/ItemService.java
@@ -1,11 +1,14 @@
 package babybeb.usersusedbookstore.service;
 
 import babybeb.usersusedbookstore.domain.Book;
+import babybeb.usersusedbookstore.domain.Member;
+import babybeb.usersusedbookstore.domain.Purchase;
 import babybeb.usersusedbookstore.domain.dealarea.DealArea;
 import babybeb.usersusedbookstore.domain.Item;
 import babybeb.usersusedbookstore.domain.ItemCondition;
 import babybeb.usersusedbookstore.repository.ItemRepository;
-import java.util.List;
+import babybeb.usersusedbookstore.repository.MemberRepository;
+import babybeb.usersusedbookstore.repository.PurchaseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +19,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class ItemService {
     
     private final ItemRepository itemRepository;
+    //    private final PurchaseService purchaseService;
+    private final PurchaseRepository purchaseRepository;
+    private final MemberRepository memberRepository;
     
     @Transactional
     public Long saveItem(Item item) {
@@ -24,7 +30,25 @@ public class ItemService {
     }
     
     @Transactional
-    public void updateItem(Long itemId, Book book, int itemPrice, ItemCondition itemCondition, DealArea dealArea) {
+    public void changeDealStatus(Long buyerId, Long itemId, String dealStatus) {
+        Item findItem = itemRepository.findOne(itemId);
+        
+        if (dealStatus.equals("COMP")) {
+            savePurchase(buyerId, findItem);
+        }
+        
+        findItem.changeDealStatus(dealStatus);
+    }
+    
+    private void savePurchase(Long buyerId, Item findItem) {
+        Member findMember = memberRepository.findOne(buyerId);
+        Purchase purchase = new Purchase(findMember, findItem);
+        purchaseRepository.save(purchase);
+    }
+    
+    @Transactional
+    public void updateItem(Long itemId, Book book, int itemPrice, ItemCondition itemCondition,
+                           DealArea dealArea) {
         Item findItem = itemRepository.findOne(itemId);
         
         findItem.changeBook(book);

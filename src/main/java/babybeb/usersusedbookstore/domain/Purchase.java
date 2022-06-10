5da550c092c61566868c2ee74d2eb6713f3cc885
babybeb <dasomchoi52@gmail.com>
2022-06-10 21:24:10 +0900

    Fix Purchase to add Item

--- src/main/java/babybeb/usersusedbookstore/domain/Purchase.java
+++ src/main/java/babybeb/usersusedbookstore/domain/Purchase.java
@@ -1,5 +1,6 @@
 package babybeb.usersusedbookstore.domain;
 
+import babybeb.usersusedbookstore.domain.dto.MemberDto;
 import javax.persistence.*;
 
 import lombok.AccessLevel;
@@ -20,7 +21,25 @@ public class Purchase {
     @JoinColumn(name = "member_id")
     Member member;
     
-//    @OneToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "item_id")
-//    Item item;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    Item item;
+    
+    public Purchase(Member member, Item item) {
+        this.member = member;
+        this.item = item;
+    }
+    
+    /* 비즈니스 로직 */
+    public void evaluate(Member seller, int score) {
+        
+        MemberDto updateMember = new MemberDto(seller.getEmail(), seller.getPassword(),
+                                               seller.getName(), seller.getNickname(),
+                                               seller.getPhoneNumber());
+        int resultRate =
+            ((seller.getSaleList().size() - 1) * seller.getRate() + score) /
+                seller.getSaleList().size();
+        updateMember.setRate(resultRate);
+        seller.updateMemberRate(updateMember);
+    }
 }

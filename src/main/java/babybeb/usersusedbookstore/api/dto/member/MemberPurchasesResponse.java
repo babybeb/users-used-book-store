c5ea3d4e2dfe69171f848aed064247e9c13da94d
ggeggrgg <ggeggrgg@naver.com>
2022-06-12 07:43:44 +0900

    Add Item id

--- src/main/java/babybeb/usersusedbookstore/api/dto/member/MemberPurchasesResponse.java
+++ src/main/java/babybeb/usersusedbookstore/api/dto/member/MemberPurchasesResponse.java
@@ -1,5 +1,6 @@
 package babybeb.usersusedbookstore.api.dto.member;
 
+import babybeb.usersusedbookstore.domain.DealStatus;
 import babybeb.usersusedbookstore.domain.Item;
 import babybeb.usersusedbookstore.domain.ItemCondition;
 import babybeb.usersusedbookstore.domain.dealarea.DealArea;
@@ -15,15 +16,17 @@ public class MemberPurchasesResponse {
     private String title;
 
     //아이템 정보
+    private Long id;
     private int price;
     private int hit;
     private ItemCondition itemCondition;
     private DealArea dealArea;
     private LocalDateTime createDate;
+    private DealStatus dealStatus;
 
     public static MemberPurchasesResponse toItemResponse(ResponseItem responseItem) {
-        return new MemberPurchasesResponse(responseItem.getBook().getTitle(), responseItem.getPrice(),
-                responseItem.getHit(), responseItem.getItemCondition(), responseItem.getDealArea(),
-                responseItem.getCreateDate());
+        return new MemberPurchasesResponse(responseItem.getBook().getTitle(), responseItem.getId(),
+                responseItem.getPrice(), responseItem.getHit(), responseItem.getItemCondition(),
+                responseItem.getDealArea(), responseItem.getCreateDate(), responseItem.getDealStatus());
     }
 }

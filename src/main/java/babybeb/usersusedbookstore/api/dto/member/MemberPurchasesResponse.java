abecaeec8b34e08fbba11b77758d66612c161059
ggeggrgg <ggeggrgg@naver.com>
2022-06-12 04:38:25 +0900

    Add Member Purchases Response

--- src/main/java/babybeb/usersusedbookstore/api/dto/member/MemberPurchasesResponse.java
+++ src/main/java/babybeb/usersusedbookstore/api/dto/member/MemberPurchasesResponse.java
@@ -1,9 +1,29 @@
 package babybeb.usersusedbookstore.api.dto.member;
 
+import babybeb.usersusedbookstore.domain.Item;
+import babybeb.usersusedbookstore.domain.ItemCondition;
+import babybeb.usersusedbookstore.domain.dealarea.DealArea;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
 public class MemberPurchasesResponse {
+    //책 정보
+    private String title;
+
+    //아이템 정보
+    private int price;
+    private int hit;
+    private ItemCondition itemCondition;
+    private DealArea dealArea;
+    private LocalDateTime createDate;
+
+    public static MemberPurchasesResponse toItemResponse(ResponseItem responseItem) {
+        return new MemberPurchasesResponse(responseItem.getBook().getTitle(), responseItem.getPrice(),
+                responseItem.getHit(), responseItem.getItemCondition(), responseItem.getDealArea(),
+                responseItem.getCreateDate());
+    }
 }

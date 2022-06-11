61883c229d65b91a1458da0c1e470ada23d00676
babybeb <dasomchoi52@gmail.com>
2022-06-11 22:50:21 +0900

    Fix ItemController to add updateItem API

--- src/main/java/babybeb/usersusedbookstore/api/ItemController.java
+++ src/main/java/babybeb/usersusedbookstore/api/ItemController.java
@@ -1,11 +1,16 @@
 package babybeb.usersusedbookstore.api;
 
 import babybeb.usersusedbookstore.api.dto.item.ItemResponse;
+import babybeb.usersusedbookstore.api.dto.item.UpdateItemRequest;
+import babybeb.usersusedbookstore.domain.Item;
 import babybeb.usersusedbookstore.service.ItemService;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,26 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class ItemController {
     
     private final ItemService itemService;
-//
-//    @PostMapping("/items")
-//    public CreateItemResponse saveItem(@RequestBody CreateItemRequest request) {
-//
-//        Item item = new Item(request.getBook(),request.getItemPrice(), request.getItemCondition(), request.getCreateDate());
-//
-//        Long id = itemService.saveItem(item);
-//
-//        return new CreateItemResponse(id);
-//    }
     
-//    @PutMapping("/items/{item_id}")
-//    public UpdateItemResponse updateMemberV2(
-//        @PathVariable("item_id") Long itemId,
-//        @RequestBody @Valid UpdateItemRequest request) {
-//
-//        itemService.updateItem(itemId, request.getName());
-//        Item findItem = itemService.findById(itemId);
-//        return new UpdateItemResponse(findMember.getId(), findMember.getName());
-//    }
+    @PutMapping("/{item_id}")
+    public ResponseEntity<ItemResponse> updateItem(
+        @PathVariable("item_id") Long itemId,
+        @RequestBody UpdateItemRequest request) {
+        
+        Item findItem = itemService.findById(itemId);
+        itemService.updateItem(itemId, findItem.getBook(), request.getItemPrice(),
+                               request.getItemCondition(), request.getDealArea());
+        
+        return ResponseEntity.ok(ItemResponse.toItemResponse(itemService.findById(itemId)));
+    }
     
     @GetMapping("/{item_id}")
     public ResponseEntity<ItemResponse> findItem(@PathVariable("item_id") Long itemId) {
